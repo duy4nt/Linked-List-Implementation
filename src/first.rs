c1ff599357737c3cd19f6962a63c66c5d6fedfb5
duy4nt <singhduyant@gmail.com>
2025-10-19 16:01:56 +0000

    Removing some errors

--- src/first.rs
+++ src/first.rs
@@ -4,27 +4,42 @@ pub struct List {
     head: Link,
 }
 
+enum Link {
+    Empty, 
+    More(Box<Node>),
+}
+
 struct Node {
     elem: i32,
     next: List,
 }
 
-enum Link {
-    Empty,
-    More(Box<Node>),
-}
-
 impl List {
     pub fn new() -> Self {
-        List{head: Link::Empty}
+        List{ head: Link::Empty }
     }
 
     pub fn push(&mut self, elem: i32) {
-        let new_node = Box::new (Node {
+        let new_node = Box::new(Node {
             elem: elem,
             next: mem::replace(&mut self.head, Link::Empty),
         });
 
         self.head = Link::More(new_node);
     }
+
+    pub fn pop(&mut self) -> Option<i32> {
+        let result;
+        match &self.head{
+            Link::Empty => {
+                result = None;
+            }
+            Link::More(node) => {
+                result = Some(node.elem);
+                self.head = node.next();
+            }
+        };
+        result
+    }
+
 }

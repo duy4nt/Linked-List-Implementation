f786bf6abdb3e9016ce9086a6eca75655795676a
duy4nt <singhduyant@gmail.com>
2025-10-21 15:33:45 +0000

    Added the Drop functionality with some errors

--- src/first.rs
+++ src/first.rs
@@ -22,24 +22,88 @@ impl List {
     pub fn push(&mut self, elem: i32) {
         let new_node = Box::new(Node {
             elem: elem,
-            next: mem::replace(&mut self.head, Link::Empty),
+            next: mem::replace(&mut self.head, first::List{head: Link::Empty}),
         });
 
         self.head = Link::More(new_node);
     }
 
     pub fn pop(&mut self) -> Option<i32> {
-        let result;
-        match &self.head{
-            Link::Empty => {
-                result = None;
-            }
+        match mem::replace(&mut self.head, Link::Empty){
+            Link::Empty => None,
             Link::More(node) => {
-                result = Some(node.elem);
-                self.head = node.next();
+                self.head = node.next;
+                Some(node.elem)
             }
-        };
-        result
+        }
     }
 
+    pub trait Drop {
+        fn drop(&mut self);
+    }
+}
+
+impl Drop for List{
+    fn drop(&mut self) {
+        let mut cur_link = mem::replace(&mut self.head, Link::Empty);
+        while let Link::More(mut boxed_node) = cur_link {
+            cur_link = mem::replace(&mut boxed_node.next, Link::Empty);
+        }
+    }
+}
+
+impl Drop for Link {
+    fn drop(&mut self) {
+        match *self {
+            Link::Empty => {}
+            Link::more(ref mut boxed_node) => {
+                boxed_node.drop();
+            }
+        }
+    }
+}
+
+impl Drop for Box<Node> {
+    fn drop(&mut self) {
+        self.ptr.drop();
+        deallocte(self.ptr);
+    }
+}
+
+impl Drop for Node {
+    fn drop(&mut self) {
+        self.next.drop();
+    }
+
+#[cfg(test)]
+mod test{
+    use super::List;
+    #[test]
+    fn basics() {
+        let mut list = List::new();
+
+        // Check empty list behaves right
+        assert_eq!(list.pop(), None);
+
+        // Populate list
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        // Check normal removal
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(2));
+
+        // Push some more just to make sure nothing's corrupted
+        list.push(4);
+        list.push(5);
+
+        // Check normal removal
+        assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), Some(4));
+
+        // Check exhaustion
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), None);
+    }
 }

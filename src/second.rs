c7fc08ffdd614df6bd8ad70e000fb13466acf75c
duy4nt <singhduyant@gmail.com>
2025-10-27 14:38:12 +0000

    Implementing iter function

--- src/second.rs
+++ src/second.rs
@@ -11,10 +11,8 @@ struct Node<T> {
 
 pub struct IntoIter<T>(List<T>);
 
-impl<T> List<T> {
-    pub fn into_iter(self) -> IntoIter<T> {
-        IntoIter(self)
-    }
+pub struct Iter<'a, T> {
+    next: Option<&'a Node<T>>,
 }
 
 impl<T> Iterator for IntoIter<T> {
@@ -25,7 +23,7 @@ impl<T> Iterator for IntoIter<T> {
     }
 }
 
-impl<T> List<T> {
+impl<'a, T> List<T> {
     pub fn new() -> Self {
         List { head: None }
     }
@@ -47,9 +45,17 @@ impl<T> List<T> {
     }
 
     pub fn peek(&mut self) -> Option<&mut T> {
-        self.head.as_mut().map(|node| {
-            &mut node.elem
-        })
+        self.head.as_mut().map(|node| &mut node.elem)
+    }
+
+    pub fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
+    }
+
+    pub fn iter(&'a self) -> Iter<'a, T> {
+        Iter {
+            next: self.head.map(|node| &'a node),
+        }
     }
 }
 
@@ -60,6 +66,17 @@ impl<T> Drop for List<T> {
         while let Some(mut boxed_node) = cur_link {
             cur_link = boxed_node.next().take();
         }
+    }
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&'a mut self) -> Option<Self::Item> {
+        self.next.map(|node| {
+            self.next = node.next.map(|node| &'a node);
+            &'a node.elem
+        })
     }
 }
 
@@ -101,14 +118,14 @@ mod test {
         let mut list = List::new();
         assert_eq!(list.peek(), None);
         assert_eq!(list.peek_mut(), None);
-        list.push(1); list.push(2); list.push(3);
+        list.push(1);
+        list.push(2);
+        list.push(3);
 
         assert_eq!(list.peek(), Some(&3));
         assert_eq!(list.peek_mut(), Some(&mut 3));
 
-        list.peek_mut().map(|value| {
-            *value = 42
-        });
+        list.peek_mut().map(|value| *value = 42);
 
         assert_eq!(list.peek(), Some(&42));
         assert_eq!(list.pop(), Some(42));
@@ -117,7 +134,9 @@ mod test {
     #[test]
     fn into_iter() {
         let mut list = List::new();
-        list.push(1); list.push(2); list.push(3);
+        list.push(1);
+        list.push(2);
+        list.push(3);
 
         let mut iter = list.into_iter();
         assert_eq!(iter.next(), Some(3));
@@ -126,4 +145,3 @@ mod test {
         assert_eq!(iter.next(), None);
     }
 }
-
